365e4059aea2e5b0d9eab3d5bc91573c00a4b3f9
Ludmila <milasasha@gmail.com>
2022-11-04 16:00:16 +0300

    feat: add code for 05 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+const fsp = require('fs/promises');
+
+async function concatFiles() {
+  const bundel = 'bundle.css';
+  dirSourse = path.resolve(__dirname, 'styles');
+  dirOutput = path.resolve(__dirname, 'project-dist');
+  dirOutputFiles = path.resolve(dirOutput, bundel);
+  const writeStream = fs.createWriteStream(dirOutputFiles, { encoding: "utf-8" });
+
+  const files = await fsp.readdir(dirSourse, { withFileTypes: true });
+
+  try {
+    for(let file of files) {
+      if(file.isFile() && path.extname(file.name) == '.css') {
+        filesSourse = path.resolve(dirSourse, file.name);
+        const contentFiles = await fsp.readFile(filesSourse, { encoding: "utf-8" });
+        writeStream.write(contentFiles + '\n');
+      }
+    }
+    console.log("Merging files was successful!");
+  } catch {
+    console.log("Don't merge files (");
+  }
+}
+concatFiles();
